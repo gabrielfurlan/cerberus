23f7f216f45b566add11cb155ef96d0b42ed7a43
Lucas Pirola <lucaspirola@gmail.com>
2018-10-21 14:18:04 -0300

    Adiciona requisicao xhr e encode de base64;

--- ui.js
+++ ui.js
@@ -1,8 +1,14 @@
 function UI() { }
 
-// UI.prototype.check = function () {
-//     window.WAPI.sendImage = function(imgBase64, chatid, filename, caption, done) {}
-// }
+function getBase64Image(img) {
+  var canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+  var dataURL = canvas.toDataURL("image/png");
+  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
 
 function handleChangeSearchInput(e) {
   const value = e.value;
@@ -35,6 +41,15 @@ UI.prototype.run = function () {
 
   const c = window.WAPI.getAllContacts();
 
+  // Carrega json da api
+  // const x = new XMLHttpRequest();
+  // const u = 'https://jsonplaceholder.typicode.com/posts';
+  // x.open("GET", u);
+  // x.send();
+  // x.onreadystatechange=(e)=>{
+  //   console.log(x.responseText)
+  // }
+  
   const r = {
     "name": "Luke",
     "images": [
@@ -51,17 +66,34 @@ UI.prototype.run = function () {
   <section id='cib-modal'>
     <h1>{{ name }}, procure seu conteúdo!</h1>
     <form id="cib-search">
-      <input onkeyup="handleChangeSearchInput(this)" oplaceholder="pesquise" id="term" name="term" />
+      <input autocomplete="off" onkeyup="handleChangeSearchInput(this)" oplaceholder="pesquise" id="term" name="term" />
       <button type="submit">GO!</button>
     </form>
     <form id="choose" class="-hide">
       <ul>
         {{#images}}
         <li class='choose-item'>
-          <input class='meme-check-input' onclick="alert('aaa')" type="radio" id="meme_{{name}}" name="meme[]" value="{{url}}" />
+          <input class='meme-check-input' type="radio" id="meme_{{name}}" name="meme[]" value="{{url}}" />
           <label for="meme_{{name}}" ><img src='{{url}}' /></label>
         </li>
         {{/images}}
+      </ul>
+      <button type="submit">buscar</button>
+    </form>
+    <h2>Destinatários</h2>
+    <form id="send">
+      <ul>
+        {{#contacts}}
+        <li key={{id._serialized}}>
+          <input
+            type="checkbox"
+            id=send-message-{{id._serialized}}
+          />
+          <label htmlFor=send-message-{{contact.id._serialized}}>
+            {{formattedName}} - {{id._serialized}}
+          </label>
+        </li>
+        {{/contacts}}
       </ul>
       <button type="submit">enviar</button>
     </form>
@@ -83,6 +115,8 @@ UI.prototype.run = function () {
 
     $('#choose').submit(function(e) {
       e.preventDefault();
+      const i = jQuery(e.currentTarget).find(":checked").parent().find("img");
+      var base64 = getBase64Image(i[0]);
       console.log('escolheu');
     });
 
@@ -94,64 +128,3 @@ UI.prototype.run = function () {
     console.log("Carregamento UI Finalizada")
   }, 3000);
 }
-
-
-// /*<h2>Resultados</h2>
-// >>>>>>> Stashed changes
-//     <form id="choose">
-//       <ul>
-//         {{#images}}
-//         <li>
-//           <input type="checkbox" id="meme_{{name}}" name="meme[]" value="{{name}}" />
-//           <img width=100 heigth=100 src='{{url}}' />
-//         </li>
-//         {{/images}}
-//       </ul>
-//       <button type="submit">enviar</button>
-//     </form>
-//     <h2>Destinatários</h2>
-//     <form id="send">
-//       <ul>
-//         {{#contacts}}
-//         <li key={{id._serialized}}>
-//           <input
-//             type="checkbox"
-//             id=send-message-{{id._serialized}}
-//           />
-//           <label htmlFor=send-message-{{contact.id._serialized}}>
-//             {{formattedName}} - {{id._serialized}}
-//           </label>
-//         </li>
-//         {{/contacts}}
-//       </ul>
-//       <button type="submit">enviar</button>
-//     </form>
-//   </section>
-// `;
-
-//   document.body.appendChild(d);
-//   document.body.appendChild(s);
-
-//     var t = $('#template').html();
-//     Mustache.parse(t);
-//     var rendered = Mustache.render(t, r);
-//     $('#cib-target').html(rendered);
-
-//     $('#cib-search').submit(function(e) {
-//       e.preventDefault();
-//       console.log('buscou');
-//     });
-
-//     $('#choose').submit(function(e) {
-//       e.preventDefault();
-//       console.log('escolheu');
-//     });
-
-//     $('#send').submit(function(e) {
-//       e.preventDefault();
-//       console.log(jQuery(e.currentTarget).find(":checked"));
-//     });
-
-//     console.log("Carregamento UI Finalizada")
-//   }, 3000);
-// }
