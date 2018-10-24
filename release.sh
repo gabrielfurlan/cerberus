712fde527fe17c968d116d03268f50d691036714
Luis Fagundes <lhfagundes@gmail.com>
2018-10-24 19:20:29 -0300

    melhoria do script de release: agora pega versao do manifest.json e faz o zip dentro de uma pasta, pra nao espalhar arquivo no computador do usuario

--- release.sh
+++ release.sh
@@ -1,2 +1,9 @@
 NODE_ENV=production npm run build
-zip dist/cerberus-v0.0.4.zip manifest.json background.js inject.js styles.css build/zapzap-app.js -r assets/*
+mkdir -p cerberus/build
+cp manifest.json background.js inject.js styles.css cerberus
+cp build/zapzap-app.js cerberus/build
+cp -r assets cerberus
+VERSION=`perl -ne 'print $1 if /^\s*.version.+?([0-9.]+)/' manifest.json`
+zip dist/cerberus-v$VERSION.zip -r cerberus
+rm -rf cerberus
+
